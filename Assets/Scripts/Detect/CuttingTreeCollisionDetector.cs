0833226f9646daee742ca90893d8c1d0c903f070
MD. NOUSHAD BHUIYAN <bmdnoushad@gmail.com>
2026-02-11 01:19:58 +0600

    Adding cutting tree mechanism

---
+++ Assets/Scripts/Detect/CuttingTreeCollisionDetector.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class CuttingTreeCollisionDetector : MonoBehaviour
+{
+    public Animator animator;
+
+    private bool hasHitThisChop = false;
+    public int hitCount = 0;
+    [SerializeField] private ParticleSystem hitParticle;
+    Rigidbody rb;
+    public float animDetectionTime = 1.5f; // Time window to detect hits during chopping animation  
+    private void Awake()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            animator = player.GetComponent<Animator>();
+        }
+        rb = GetComponent<Rigidbody>();
+        // Find AxeEffect in hierarchy
+        GameObject effectObj = GameObject.Find("AxeEffect");
+        if (effectObj != null)
+        {
+            hitParticle = effectObj.GetComponent<ParticleSystem>();
+        }
+    }
+    private void Update()
+    {
+        // If animation finished, reset hit flag
+        if (!IsChopping())
+        {
+            hasHitThisChop = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!IsChopping()) return;
+        if (hasHitThisChop) return;
+
+        if (collision.gameObject.CompareTag("TreeHitPoint"))
+        {
+            Debug.Log("Tree Hit During Chopping!");
+            ParticleSystem p= Instantiate(hitParticle, collision.gameObject.transform,collision.transform);
+            p.Play();
+            hitCount++;
+            hasHitThisChop = true;
+            if (hitCount == 3)
+            {
+                RotateTree(this.gameObject);
+                rb.isKinematic = false;
+                //spawn tree wood prefab here
+            }
+            Destroy(p.gameObject, 4f);
+        }
+    }
+    private void RotateTree(GameObject treeObject)
+    {
+        Vector3 newRotation = treeObject.transform.eulerAngles;
+        newRotation.x = -1f;
+
+        treeObject.transform.eulerAngles = newRotation;
+
+        Debug.Log("Tree rotated on X axis!");
+    }
+
+
+    bool IsChopping()
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        return stateInfo.IsName("Chopping") && stateInfo.normalizedTime < animDetectionTime;
+    }
+}
